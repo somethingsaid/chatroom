ca4178cd805dd50b66dbb7ef82d4ab55d382c57e
somethingsaid <kwong.billy@gmail.com>
2016-07-29 10:36:46 -0400

    Added user nicknames, user is typing

--- public/main.js
+++ public/main.js
@@ -1,22 +1,85 @@
 $(document).ready(function() {
+
+    // prompt for nickname and set to socket
+    var nickName = null;
+    while (!nickName) {
+        nickName = window.prompt("Please enter your nickname:", "");
+    }
+
+    $('#nickName').html(nickName);
+
     var socket = io();
+    socket.emit('regUser', nickName);
+
     var input = $('input');
     var messages = $('#messages');
+    var clientCount = $('#clientCount');
+    var typing = $('#userTyping');
 
-    var addMessage = function(message) {
+    // add message to message area
+    var addMessage = function(message, includeNickName) {
+
+        // look for pm|{username}|{message} format
+        var args = message.split('|');
+        if (args.length === 3) {
+            message = args[2];
+        }
+        if (includeNickName) {
+            message = nickName + ': ' + message;
+        }
         messages.append('<div>' + message + '</div>');
     };
 
+    // update client count
+    var updClients = function(clients) {
+        clientCount.html('Connected Clients = ' + Object.keys(clients).length);
+
+        $("ul").empty();
+        var html = "";
+        for(var index in clients) {
+            if (clients.hasOwnProperty(index)) {
+                html += "<li>" + clients[index] + "</li>";
+            }
+        }
+        $("ul").html(html);
+    };
+
+    // update {username} is typing div
+    var userTyping = function(nickName) {
+        typing.html(nickName + ' is typing.');
+    };
+
+    // clear {username} is typing div
+    var clearUserTyping = function() {
+        typing.html('');
+    };
+
+    // socket event handlers
+    socket.on('message', addMessage);
+    socket.on('clientsChanged', updClients);
+    socket.on('keydown', userTyping);
+    socket.on('keyup', clearUserTyping);
+
+    // on keydown, if enter, send message.
+    // otherwise emit 'keydown' event
     input.on('keydown', function(event) {
         if (event.keyCode != 13) {
+            console.log('keydown');
+            socket.emit('keydown');
             return;
         }
 
         var message = input.val();
-        addMessage(message);
+        addMessage(message, true);
         socket.emit('message', message);
         input.val('');
     });
 
-    socket.on('message', addMessage);
+    // on keyup, emit 'keyup' event if not enter
+    input.on('keyup', function(event) {
+        if (event.keyCode != 13) {
+            socket.emit('keyup');
+        }
+    });
+
 });
--- server.js
+++ server.js
@@ -8,12 +8,55 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+// keep copy of connected clients
+// use socket id, and nickName
+var clients = new Object();
+
+// get client id by nickname
+var getClientID = function(nickName) {
+    var id = null;
+    for(var index in clients) {
+        if (clients.hasOwnProperty(index)) {
+            if (clients[index] === nickName) {
+                id = index;
+                break;
+            }
+        }
+    }
+    return id;
+};
+
 io.on('connection', function (socket) {
-    console.log('Client connected');
+    socket.on('regUser', function(nickName) {
+        clients[socket.id] = nickName; // identify user by socket.id
+        socket.broadcast.emit('message', nickName + ' has joined the chat.');
+        io.emit('clients changed', clients);
+    });
+
 
     socket.on('message', function(message) {
+        var sender = clients[socket.id];
         console.log('Received message:', message);
-        socket.broadcast.emit('message', message);
+        socket.broadcast.emit('message', sender + ": " + message);
+    });
+
+    socket.on('disconnect', function() {
+        if (!clients[socket.id]) {
+            return;
+        }
+        var user = clients[socket.id];
+        delete clients[socket.id];
+        socket.broadcast.emit('message', user + ' has left the chat.');
+        io.emit('clients changed', clients);
+    });
+    
+    socket.on('keydown', function() {
+        var nickName = clients[socket.id];
+        socket.broadcast.emit('keydown', nickName);
+    });
+
+    socket.on('keyup', function() {
+        socket.broadcast.emit('keyup');
     });
 });
 
